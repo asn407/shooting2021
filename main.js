c7e0da7cd225cf38b72f655980458c9f583750f6
asn407 <s21t317@kagawa-u.ac.jp>
2024-12-14 22:35:26 +0900

    fix: やめ．あたまおかしくなる

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // 厳格モードで実行
 "use strict";
-// 2dグラフィックを描写するためのオブジェクトを格納するグローバル変数
+// 2Dグラフィック描写のオブジェクト
 let context;
+// キー押下情報
+let keyFlag = [];
+
+let loopReqest = null;
+
+window.addEventListener("keydown", (e) => {
+    keyFlag[e.key] = true;
+});
+
+window.addEventListener("keyup", (e) => {
+    keyFlag[e.key] = false;
+});
+
+
 
 class Block
 {
@@ -29,7 +43,36 @@ class Player extends Block
     }
 
     move()
-    {}
+    {
+        if (keyFlag["ArrowLeft"] === true)
+        {
+            this.x--;
+        }
+        if (keyFlag["ArrowUp"])    this.y--;
+        if (keyFlag["ArrowRight"]) this.x++;
+        if (keyFlag["ArrowDown"])  this.y++;
+        if (keyFlag["w"])          this.x--;
+        if (keyFlag["a"])          this.y--;
+        if (keyFlag["s"])          this.x++;
+        if (keyFlag["d"])          this.y++;
+    }
+}
+
+
+
+function main()
+{
+    // 図形描写の初期化
+    let canvas = document.getElementById("canvas");
+    // 2Dグラフィック描写オブジェクトを取得
+    context = canvas.getContext("2d");
+
+    let player = new Player();
+}
+
+function loop()
+{
+    player.draw();
 }
 
 class Main
@@ -38,23 +81,25 @@ class Main
     {
         // 図形描写の初期化
         let canvas = document.getElementById("canvas");
-        // 2dグラフィックを描写するためのオブジェクトを取得
+        // 2Dグラフィック描写のオブジェクトを取得
         context = canvas.getContext("2d");
 
         this.loopReqest = null;
         this.player = new Player();
-        this.update()
+
+        this.loop();
     }
 
     loop()
     {
-        context.clearRect(0, 0, 440, 440);
         this.update();
-        this.loopReqest = window.requestAnimationFrame(this.loop);
+        // context.clearRect(0, 0, 440, 440);
+        this.loopReqest = window.requestAnimationFrame(this.loop());
     }
 
     update()
     {
+        this.player.move();
         this.player.draw();
     }
 }
